500ebe7b0971b487ff202f9ace5dcca8ccc4bd69
luanakimley <luanaa.kimley@gmail.com>
2023-11-25 12:54:52 +0000

    :hammer: Make landing screen default on launch screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:play_metrix/screens/home_screen.dart';
-import 'package:play_metrix/screens/player_profile_screen.dart';
-import 'package:play_metrix/screens/team_profile_screen.dart';
 import 'screens/landing_screen.dart'; // Import your landing page
 
 void main() {
@@ -18,7 +15,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
           // Define your app's theme here
           ),
-      home: const PlayerProfileScreen(), // Set your landing page here
+      home: const LandingScreen(), // Set your landing page here
     );
   }
 }
