3805e9853d066d9e41ca6a8935496633efc28593
Nathan Field <d00235179@student.dkit.ie>
2023-11-25 19:38:33 +0000

    :ambulance: Update Crud Functions

--- crud.py
+++ crud.py
@@ -1,14 +1,29 @@
 
 from sqlalchemy.orm import Session
-
-import models
-
+from fastapi import Depends, FastAPI, HTTPException
+from models import *
 
 
 def get_leagues(db: Session):
     try:
-        leagues = db.query(models.League).all()
+        leagues = db.query(league).all()
         return leagues
     except Exception as e:
         print(f"Error retrieving leagues: {e}")
         return []
+    
+# def delete_player(db: Session):
+#     try:
+#         users = db.query(player_login).all()
+#         if not users:
+#             raise HTTPException(status_code=404, detail="No Players found")
+        
+#         for user in users:
+#             db.delete(user)
+        
+#         db.commit()
+#         return {"message": "All Players deleted successfully"}
+#     except Exception as e:
+#         print(f"Error deleting players: {e}")
+#         return []
+
