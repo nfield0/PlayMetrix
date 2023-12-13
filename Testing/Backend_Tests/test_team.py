969adf29b0fb6a91eb5bfaf9cc8852d3c7d503ce
Nathan Field <d00235179@student.dkit.ie>
2023-12-13 20:34:26 +0000

    :sparkles: Test Team

---
+++ Testing/Backend_Tests/test_team.py
@@ -0,0 +1,93 @@
+import requests
+
+
+
+def test_a_cleanup():
+    url = 'http://127.0.0.1:8000/cleanup_tests'
+    headers = {'Content-Type': 'application/json'}
+    response = requests.delete(url, headers=headers)
+    assert response.status_code == 200
+
+
+
+def test_adc_manager():
+    url = 'http://127.0.0.1:8000/register'
+    headers = {'Content-Type': 'application/json'}
+    json = {
+        "user_type": "manager",
+        "user_email": "team_manager@gmail.com",
+        "user_password": "Password123!"
+    }
+    response = requests.post(url, headers=headers, json=json)
+    
+    assert response.headers['Content-Type'] == 'application/json'
+
+    try:
+        response_json = response.json()
+        assert response_json.get("detail") == "Manager Registered Successfully"
+        assert 'id' in response_json
+        assert response_json['id']['manager_id'] == 1
+        assert response.status_code == 200
+    
+    except (ValueError, AssertionError) as e:
+        assert False, f"Test failed: {e}"
+
+
+def test_add_league():
+    url = 'http://127.0.0.1:8000/leagues/'
+    headers = {'Content-Type': 'application/json'}
+    json = {
+        "league_name": "Louth GAA",
+        "league_logo": "b'url"
+    }
+    response = requests.post(url, headers=headers, json=json)
+
+    # assert response.headers['Content-Type'] == 'application/json'
+
+    try:
+        response_json = response.json()
+        assert response_json.get("detail") == "League inserted successfully"
+        assert 'id' in response_json
+        assert response_json['id']['league_id'] == 1
+        assert response.status_code == 200  
+
+    except (ValueError, AssertionError) as e:
+        assert False, f"Test failed: {e}"
+
+def test_add_team():
+    url = 'http://127.0.0.1:8000/teams/'
+    headers = {'Content-Type': 'application/json'}
+    json = {
+        "team_name": "Louth GAA",
+        "team_logo": "b'url",
+        "manager_id": 1,
+        "league_id": 1,
+        "sport_id": 1,
+        "team_location": "Dundalk, Louth"
+    }
+    response = requests.post(url, headers=headers, json=json)
+    
+    assert response.headers['Content-Type'] == 'application/json'
+
+    try:
+        response_json = response.json()
+        assert response_json == "Team inserted successfully"
+        assert 'id' in response_json
+        assert response_json['id']['team_id'] == 1
+        assert response.status_code == 200
+    
+    except (ValueError, AssertionError) as e:
+        assert False, f"Test failed: {e}"
+
+
+
+
+
+
+
+def test_z_cleanup():
+    url = 'http://127.0.0.1:8000/cleanup_tests'
+    headers = {'Content-Type': 'application/json'}
+    response = requests.delete(url, headers=headers)
+    assert response.status_code == 200
+
