0d49203511d7eb90010b4318bc92f148568e3eb0
Nathan Field <d00235179@student.dkit.ie>
2024-01-18 11:07:58 +0000

    :sparkles: Add team_physio tests

--- Testing/Backend_Tests/test_physio.py
+++ Testing/Backend_Tests/test_physio.py
@@ -201,6 +201,123 @@ def test_delete_physio():
     except (ValueError, AssertionError) as e:
         assert False, f"Test failed: {e}"
 
+# team_physio tests
+
+def test_add_team_manager():
+    url = 'http://127.0.0.1:8000/register_manager'
+    headers = {'Content-Type': 'application/json'}
+    json = {
+        "manager_email": "testmanager@gmail.com",
+        "manager_password": "Password123!",
+        "manager_firstname": "test",
+        "manager_surname": "tester",
+        "manager_contact_number": "012345",
+        "manager_image": "something"
+    
+    }
+    response = requests.post(url, headers=headers, json=json)
+    
+    assert response.headers['Content-Type'] == 'application/json'
+    assert response.status_code == 200
+
+    try:
+        response_json = response.json()
+        assert response_json.get("detail") == "Manager Registered Successfully"
+        assert 'id' in response_json
+        assert response_json['id']['manager_id'] == 1
+    
+    except (ValueError, AssertionError) as e:
+        assert False, f"Test failed: {e}"
+
+
+def test_add_league():
+    url = 'http://127.0.0.1:8000/leagues/'
+    headers = {'Content-Type': 'application/json'}
+    json = {
+        "league_name": "Louth GAA"
+    }
+    response = requests.post(url, headers=headers, json=json)
+
+    # assert response.headers['Content-Type'] == 'application/json'
+
+    try:
+        response_json = response.json()
+        assert response_json.get("message") == "League inserted successfully"
+        assert 'id' in response_json
+        assert response_json['id'] == 1
+        assert response.status_code == 200  
+
+    except (ValueError, AssertionError) as e:
+        assert False, f"Test failed: {e}"
+
+def test_add_sport():
+    url = 'http://127.0.0.1:8000/sports/'
+    headers = {'Content-Type': 'application/json'}
+    json = {
+        "sport_name": "Gaelic Rugby"
+    }
+    response = requests.post(url, headers=headers, json=json)
+
+    assert response.headers['Content-Type'] == 'application/json'
+
+    try:
+        response_json = response.json()
+        assert response_json.get("message") == "Sport inserted successfully"
+        assert 'id' in response_json
+        assert response_json['id'] == 1
+        assert response.status_code == 200  
+
+    except (ValueError, AssertionError) as e:
+        assert False, f"Test failed: {e}"
+
+def test_add_team_for_physio_tests():
+    url = 'http://127.0.0.1:8000/teams/'
+    headers = {'Content-Type': 'application/json'}
+    json = {
+        "team_name": "Louth Under 21s GAA",
+        "team_logo": "b'url",
+        "manager_id": 1,
+        "league_id": 1,
+        "sport_id": 1,
+        "team_location": "Dundalk, Louth"
+    }
+    response = requests.post(url, headers=headers, json=json)
+    
+    assert response.headers['Content-Type'] == 'application/json'
+
+    try:
+        response_json = response.json()
+        assert response_json.get("message") == "Team inserted successfully"
+        assert 'id' in response_json
+        assert response_json['id'] == 1
+        assert response.status_code == 200
+    
+    except (ValueError, AssertionError) as e:
+        assert False, f"Test failed: {e}"
+
+def test_add_physio():
+    url = 'http://127.0.0.1:8000/register_physio'
+    headers = {'Content-Type': 'application/json'}
+    json = {
+        "physio_email": "testphysio@gmail.com",
+        "physio_password": "Password123!",
+        "physio_firstname": "test",
+        "physio_surname": "tester",
+        "physio_contact_number": "012345"
+    }
+    response = requests.post(url, headers=headers, json=json)
+    assert response.status_code == 200
+    assert response.headers['Content-Type'] == 'application/json'
+
+    try:
+        response_json = response.json()
+        assert response_json.get("detail") == "Physio Registered Successfully"
+        assert 'id' in response_json
+        assert response_json['id']['physio_id'] == 1
+    
+    except(ValueError, AssertionError) as e:
+        assert False, f"Test failed: {e}"
+
 
 def test_add_team_physio():
     url = 'http://127.0.0.1:8000/team_physio'
