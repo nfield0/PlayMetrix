44c9495a7889a4befd64fde4a560a594097830e3
luanakimley <luanaa.kimley@gmail.com>
2024-01-20 17:26:25 +0000

    :sparkles: Develop functionality of setting up manager profile page

--- Frontend/lib/screens/profile/profile_set_up.dart
+++ Frontend/lib/screens/profile/profile_set_up.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:play_metrix/constants.dart';
+import 'package:play_metrix/screens/authentication/log_in_screen.dart';
 import 'package:play_metrix/screens/authentication/sign_up_choose_type_screen.dart';
 import 'package:play_metrix/screens/home_screen.dart';
 import 'package:play_metrix/screens/team/team_profile_screen.dart';
@@ -10,54 +11,101 @@ import 'package:play_metrix/screens/widgets/buttons.dart';
 import 'package:play_metrix/screens/widgets/common_widgets.dart';
 import 'dart:convert';
 import 'package:http/http.dart' as http;
+import 'dart:typed_data';
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
-Future<void> updateManagerProfile(String contactNumber, String image) async {
-final apiUrl =
-        'http://127.0.0.1:8000/register_manager'; // Replace with your actual backend URL
+final profilePictureProvider = StateProvider<Uint8List?>((ref) => null);
 
-    try {
-      final response = await http.post(
-        Uri.parse(apiUrl),
-        headers: <String, String>{
-          'Content-Type': 'application/json; charset=UTF-8',
-        },
-        body: jsonEncode(<String, dynamic>{
-          // 'manager_firstname': firstName,
-          // 'manager_surname': surname,
-          // 'manager_email': email,
-          // 'manager_password': password,
-          'manager_contact_number': "",
-          'manager_image': "",
-        }),
-      );
+class ProfileName {
+  final String firstName;
+  final String surname;
 
-      if (response.statusCode == 200) {
-        // Successfully registered, handle the response accordingly
-        print('Registration successful!');
-        print('Response: ${response.body}');
+  ProfileName(this.firstName, this.surname);
+}
 
-      } else {
-        // Failed to register, handle the error accordingly
-        print('Failed to register. Status code: ${response.statusCode}');
-        print('Error message: ${response.body}');
-      }
-    } catch (error) {
-      // Handle any network or other errors
-      print('Error: $error');
+Future<ProfileName> getManagerProfile(int id) async {
+  final apiUrl = 'http://127.0.0.1:8000/managers/$id';
+  try {
+    final response =
+        await http.get(Uri.parse(apiUrl), headers: <String, String>{
+      'Content-Type': 'application/json; charset=UTF-8',
+    });
+
+    if (response.statusCode == 200) {
+      print('Response: ${response.body}');
+      final parsed = jsonDecode(response.body);
+
+      return ProfileName(
+          parsed['manager_firstname'], parsed['manager_surname']);
+    } else {
+      print('Error message: ${response.body}');
     }
+  } catch (error) {
+    print('Error: $error');
+  }
+
+  return ProfileName("", "");
+}
+
+Future<void> updateManagerProfile(int id, ProfileName name,
+    String contactNumber, Uint8List? imageBytes) async {
+  final apiUrl =
+      'http://127.0.0.1:8000/managers/info/$id'; // Replace with your actual backend URL
+
+  try {
+    final response = await http.put(
+      Uri.parse(apiUrl),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+      body: jsonEncode(<String, dynamic>{
+        'manager_id': id,
+        'manager_firstname': name.firstName,
+        'manager_surname': name.surname,
+        'manager_contact_number': contactNumber,
+        'manager_image': base64Encode(imageBytes!),
+      }),
+    );
+
+    if (response.statusCode == 200) {
+      // Successfully registered, handle the response accordingly
+      print('Registration successful!');
+      print('Response: ${response.body}');
+    } else {
+      // Failed to register, handle the error accordingly
+      print('Failed to register. Status code: ${response.statusCode}');
+      print('Error message: ${response.body}');
+    }
+  } catch (error) {
+    // Handle any network or other errors
+    print('Error: $error');
+  }
 }
 
 Future<void> updatePhysioProfile() async {}
 
 Future<void> updateCoachProfile() async {}
 
-
 class ProfileSetUpScreen extends ConsumerWidget {
-  String selectedDivisionValue = "Division 1";
+  final TextEditingController _phoneController = TextEditingController();
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    Future<void> pickImage() async {
+      final picker = ImagePicker();
+      final pickedFile = await picker.pickImage(source: ImageSource.gallery);
+
+      if (pickedFile != null) {
+        List<int> imageBytes = await pickedFile.readAsBytes();
+
+        ref.read(profilePictureProvider.notifier).state =
+            Uint8List.fromList(imageBytes);
+      }
+    }
+
     final userRole = ref.watch(userRoleProvider.notifier).state;
+    Uint8List? profilePicture = ref.watch(profilePictureProvider);
 
     return Scaffold(
       appBar: AppBar(
@@ -93,18 +141,33 @@ class ProfileSetUpScreen extends ConsumerWidget {
             const SizedBox(height: 20),
             Center(
                 child: Column(children: [
-              Image.asset(
-                "lib/assets/icons/profile_placeholder.png",
-                width: 100,
-              ),
+              profilePicture != null
+                  ? Image.memory(
+                      profilePicture,
+                      width: 100,
+                    )
+                  : Image.asset(
+                      "lib/assets/icons/profile_placeholder.png",
+                      width: 100,
+                    ),
               const SizedBox(height: 10),
-              underlineButtonTransparent("Upload picture", () {}),
+              underlineButtonTransparent("Upload picture", () {
+                pickImage();
+              }),
             ])),
             const SizedBox(height: 20),
-            formFieldBottomBorder("Phone", ""),
+            formFieldBottomBorderController("Phone", _phoneController),
             const SizedBox(height: 50),
-            bigButton("Save Changes", () {
+            bigButton("Save Changes", () async {
               if (userRole == UserRole.manager) {
+                int userId = ref.watch(userIdProvider.notifier).state;
+                ProfileName managerName = await getManagerProfile(userId);
+
+                await updateManagerProfile(
+                    userId,
+                    managerName,
+                    _phoneController.text,
+                    ref.read(profilePictureProvider.notifier).state);
                 Navigator.push(
                   context,
                   MaterialPageRoute(builder: (context) => TeamSetUpScreen()),
