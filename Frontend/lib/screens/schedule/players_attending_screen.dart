54f89a59d2f59d5d707dd2a30d5a4a9f0d2f0fb1
conor begley <Conorbegley5@gmail.com>
2024-01-28 19:33:21 +0000

    In this commit, I added the code for the edit schedule screen, the player attending screen and schedule details screen. Having issues with the schedule details screens and the player attending screen, unsure of how to get them fully working, but http request functions are implemented. Also when testing I tried getting the add schedule to work and it wasn't working either

--- Frontend/lib/screens/schedule/players_attending_screen.dart
+++ Frontend/lib/screens/schedule/players_attending_screen.dart
@@ -1,24 +1,116 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:play_metrix/constants.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:play_metrix/screens/player/player_profile_screen.dart';
 import 'package:play_metrix/screens/player/players_screen.dart';
 import 'package:play_metrix/screens/widgets/bottom_navbar.dart';
 import 'package:play_metrix/screens/widgets/common_widgets.dart';
+import 'package:play_metrix/screens/schedule/schedule_details_screen.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'dart:typed_data';
 
-class PlayersAttendingScreen extends StatefulWidget {
-  const PlayersAttendingScreen({Key? key}) : super(key: key);
+class Schedule {
+  int schedule_id;
+  String schedule_title;
+  String schedule_location;
+  String schedule_type;
+  String schedule_start_time;
+  String schedule_end_time;
+  String schedule_alert_time;
 
-  @override
-  _PlayersAttendingScreenState createState() => _PlayersAttendingScreenState();
+  Schedule(
+      {required this.schedule_id,
+      required this.schedule_title,
+      required this.schedule_location,
+      required this.schedule_type,
+      required this.schedule_start_time,
+      required this.schedule_end_time,
+      required this.schedule_alert_time});
+
+  factory Schedule.fromJson(Map<String, dynamic> json) {
+    return Schedule(
+        schedule_id: json['schedule_id'],
+        schedule_title: json['schedule_title'],
+        schedule_location: json['schedule_location'],
+        schedule_type: json['schedule_type'],
+        schedule_start_time: json['schedule_start_time'],
+        schedule_end_time: json['schedule_end_time'],
+        schedule_alert_time: json['schedule_alert_time']);
+  }
 }
 
-class _PlayersAttendingScreenState extends State<PlayersAttendingScreen> {
+Future<Schedule> getScheduleById(int scheduleId) async {
+  final apiUrl = '$apiBaseUrl/schedules/$scheduleId';
+  try {
+    final response = await http.get(Uri.parse(apiUrl));
+    if (response.statusCode == 200) {
+      print('Get players attending successful!');
+      print('Response: ${response.body}');
+
+      Schedule schedule = Schedule.fromJson(jsonDecode(response.body));
+      return schedule;
+    } else {
+      // Failed to retrieve data, handle the error accordingly
+      print('Failed to retrieve data. Status code: ${response.statusCode}');
+      print('Error message: ${response.body}');
+    }
+  } catch (error) {
+    // Handle any network or other errors
+    print("user");
+    print('Error: $error');
+  }
+  throw Exception('Failed to players attending data');
+}
+
+class playerAttending {
+  int schedule_id;
+  int player_id;
+  bool player_attending;
+
+  playerAttending(
+      {required this.schedule_id,
+      required this.player_id,
+      required this.player_attending});
+
+  factory playerAttending.fromJson(Map<String, dynamic> json) {
+    return playerAttending(
+        schedule_id: json['schedule_id'],
+        player_id: json['player_id'],
+        player_attending: json['player_attending']);
+  }
+}
+
+
+
+// class PlayersAttendingScreen extends StatefulWidget {
+//   const PlayersAttendingScreen({Key? key}) : super(key: key);
+
+//   @override
+//   _PlayersAttendingScreenState createState() => _PlayersAttendingScreenState();
+// }
+
+class PlayersAttendingScreen extends ConsumerWidget {
+  late Schedule schedule;
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
+  Widget build(BuildContext context, WidgetRef ref) {
+    int scheduleId = ref.watch(scheduleIdProvider);
+    return FutureBuilder<Schedule>(
+	    future: getScheduleById(scheduleId),
+      builder: (context, snapshot) {
+	    if (snapshot.connectionState == ConnectionState.waiting) {
+        return CircularProgressIndicator();
+      } else if (snapshot.hasError) {
+		    return Text('Error: ${snapshot.error}');
+      } else if (snapshot.hasData) {
+		    schedule = snapshot.data!;
+        String schedule_title = schedule.schedule_title;
+        String schedule_type = schedule.schedule_type;
+        String schedule_start_time = schedule.schedule_start_time;
+      return Scaffold(
         appBar: AppBar(
-          title: appBarTitlePreviousPage("{Event Name}"),
+          title: appBarTitlePreviousPage("${schedule_title}"),
           iconTheme: const IconThemeData(
             color: AppColours.darkBlue, //change your color here
           ),
@@ -36,8 +128,9 @@ class _PlayersAttendingScreenState extends State<PlayersAttendingScreen> {
                     fontSize: 28,
                     fontWeight: FontWeight.bold,
                     fontFamily: AppFonts.gabarito)),
-            const SizedBox(height: 5),
-            const Text("Training | Tuesday, 2 October 2023"),
+            const SizedBox(height: 5), 
+            Text("${schedule_type} | ${schedule_start_time}"),
+            
             const SizedBox(height: 30),
             // playerProfilePill(
             //     context,
@@ -56,6 +149,11 @@ class _PlayersAttendingScreenState extends State<PlayersAttendingScreen> {
             //     AvailabilityStatus.Available),
           ])),
         )),
-        bottomNavigationBar: managerBottomNavBar(context, 2));
+        bottomNavigationBar: managerBottomNavBar(context, 2)
+        );
+        } else {
+            return Text('No data available');
+          }
+        });
   }
 }
