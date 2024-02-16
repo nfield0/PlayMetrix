37fe6da2435f3db337e0c9f44557223962302e13
Nathan Field <d00235179@student.dkit.ie>
2024-02-16 22:14:32 +0000

    :sparkles: Basic tests for Notification Routes (subject to change)

--- Testing/Backend_Tests/test_notification.py
+++ Testing/Backend_Tests/test_notification.py
@@ -21,5 +21,55 @@ def test_add_notification():
     except (ValueError, AssertionError) as e:
         assert False, f"Test failed: {e}"
 
+def test_get_notification():
+    url = 'http://127.0.0.1:8000/notification/1'
+    headers = {'Content-Type': 'application/json'}
+    
+    response = requests.get(url, headers=headers)
+    try:
+        expected_json = {
+        "notification_id": 1,
+        "notification_title": "Test Notification",
+        "notification_desc": "Test Description",
+        "notification_date": "2024-01-21T00:00:00",
+        "team_id": 1,
+        "poster_id": 1,
+        "poster_type": "manager"
+    }
+        response_json = response.json()
+        assert response_json == expected_json
+        assert response.status_code == 200
+    except (ValueError, AssertionError) as e:
+        assert False, f"Test failed: {e}"
+
+def test_update_notification():
+    url = 'http://127.0.0.1:8000/notification/1'
+    headers = {'Content-Type': 'application/json'}
+    json = {
+        "notification_title": "Test Notification Updated",
+        "notification_desc": "Test Description Updated",
+        "notification_date": "2024-01-21T00:00:00",
+        "team_id": 1,
+        "poster_id": 1,
+        "poster_type": "manager"
+    }
+    response = requests.put(url, headers=headers, json=json)
+    try:
+        response_json = response.json()
+        assert response_json.get("message") == "Notification with ID 1 has been updated"
+        assert response.status_code == 200
+    except (ValueError, AssertionError) as e:
+        assert False, f"Test failed: {e}"
 
 
+def test_delete_notification():
+    url = 'http://127.0.0.1:8000/notification/1'
+    headers = {'Content-Type': 'application/json'}
+    
+    response = requests.delete(url, headers=headers)
+    try:
+        response_json = response.json()
+        assert response_json.get("message") == "Notification deleted successfully"
+        assert response.status_code == 200
+    except (ValueError, AssertionError) as e:
+        assert False, f"Test failed: {e}"
