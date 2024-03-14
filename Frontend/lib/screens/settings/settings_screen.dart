dc73ca32ca95727d921e0b3c8aca854abf00465a
luanakimley <luanaa.kimley@gmail.com>
2024-03-14 20:31:46 +0000

    :sparkles: Develop structure of 2fa page

--- Frontend/lib/screens/settings/settings_screen.dart
+++ Frontend/lib/screens/settings/settings_screen.dart
@@ -6,6 +6,7 @@ import 'package:play_metrix/providers/user_provider.dart';
 import 'package:play_metrix/screens/profile/edit_profile_screen.dart';
 import 'package:play_metrix/screens/profile/profile_screen.dart';
 import 'package:play_metrix/screens/settings/change_password_screen.dart';
+import 'package:play_metrix/screens/settings/two_fa_settings_screen.dart';
 import 'package:play_metrix/screens/widgets_lib/bottom_navbar.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:play_metrix/screens/widgets_lib/common_widgets.dart';
@@ -63,17 +64,18 @@ class SettingsScreen extends ConsumerWidget {
                                               ref.read(userRoleProvider))),
                                 );
                               }),
-                              settingsRow(
-                                  "Change password", Icons.key_outlined, () {
-                                    Navigator.push(
+                              settingsRow("Change password", Icons.key_outlined,
+                                  () {
+                                Navigator.push(
                                   context,
                                   MaterialPageRoute(
-                                      builder: (context) => ChangePasswordScreen(
-                                          userId: ref.read(userIdProvider),
-                                          userRole:
-                                              ref.read(userRoleProvider))),
+                                      builder: (context) =>
+                                          ChangePasswordScreen(
+                                              userId: ref.read(userIdProvider),
+                                              userRole:
+                                                  ref.read(userRoleProvider))),
                                 );
-                                  }),
+                              }),
                               Padding(
                                 padding:
                                     const EdgeInsets.symmetric(horizontal: 20),
@@ -82,8 +84,17 @@ class SettingsScreen extends ConsumerWidget {
                               const SizedBox(height: 10),
                               sectionHeader("Security"),
                               const SizedBox(height: 15),
-                              settingsRow("Two-factor authentication",
-                                  Icons.lock, () {}),
+                              settingsRow(
+                                  "Two-factor authentication", Icons.lock, () {
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) => TwoFASettingsScreen(
+                                          userId: ref.read(userIdProvider),
+                                          userRole:
+                                              ref.read(userRoleProvider))),
+                                );
+                              }),
                               Padding(
                                 padding:
                                     const EdgeInsets.symmetric(horizontal: 20),
---
+++ Frontend/lib/screens/settings/two_fa_settings_screen.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+import 'package:play_metrix/constants.dart';
+import 'package:play_metrix/enums.dart';
+import 'package:play_metrix/screens/settings/settings_screen.dart';
+import 'package:play_metrix/screens/widgets_lib/common_widgets.dart';
+
+class TwoFASettingsScreen extends StatefulWidget {
+  final UserRole userRole;
+  final int userId;
+
+  const TwoFASettingsScreen(
+      {super.key, required this.userRole, required this.userId});
+
+  @override
+  TwoFASettingsScreenState createState() => TwoFASettingsScreenState();
+}
+
+class TwoFASettingsScreenState extends State<TwoFASettingsScreen> {
+  bool twoFaOn = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          "Two-Factor Authentication",
+          style: TextStyle(
+            fontFamily: AppFonts.gabarito,
+            color: AppColours.darkBlue,
+            fontWeight: FontWeight.bold,
+            fontSize: 24,
+          ),
+        ),
+        iconTheme: const IconThemeData(
+          color: AppColours.darkBlue, //change your color here
+        ),
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+      ),
+      body: SingleChildScrollView(
+          child: ConstrainedBox(
+              constraints: const BoxConstraints(maxWidth: 800),
+              child: Container(
+                  padding: const EdgeInsets.all(30),
+                  child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Container(
+                            padding: const EdgeInsets.symmetric(
+                                horizontal: 20, vertical: 15),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                const Text(
+                                  "Enable 2FA",
+                                  style: TextStyle(
+                                      fontSize: 17,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                                Switch(
+                                  // This bool value toggles the switch.
+                                  value: twoFaOn,
+                                  activeColor: AppColours.darkBlue,
+                                  onChanged: (bool value) {
+                                    // This is called when the user toggles the switch.
+                                    setState(() {
+                                      twoFaOn = value;
+                                    });
+                                  },
+                                )
+                              ],
+                            )),
+                        const SizedBox(height: 10),
+                        greyDividerThick(),
+                        const SizedBox(height: 20),
+                        const Text(
+                            "Two-factor authentication keeps your account safe by asking you to enter a verification code sent to your phone every time you log in.",
+                            style: TextStyle(fontSize: 15)),
+                      ])))),
+    );
+  }
+}
