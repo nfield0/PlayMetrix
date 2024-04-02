1368ffeaaaac237ea10fb7e896e9dbd69a5d11de
luanakimley <luanaa.kimley@gmail.com>
2024-04-02 23:32:52 +0100

    :lipstick: :bug: Make change password and 2fa screen centred

--- Frontend/lib/screens/settings/two_fa_settings_screen.dart
+++ Frontend/lib/screens/settings/two_fa_settings_screen.dart
@@ -3,6 +3,7 @@ import 'package:play_metrix/api_clients/authentication_api_client.dart';
 import 'package:play_metrix/constants.dart';
 import 'package:play_metrix/enums.dart';
 import 'package:play_metrix/screens/profile/profile_screen.dart';
+import 'package:play_metrix/screens/widgets_lib/bottom_navbar.dart';
 import 'package:play_metrix/screens/widgets_lib/common_widgets.dart';
 
 class TwoFASettingsScreen extends StatefulWidget {
@@ -49,45 +50,48 @@ class TwoFASettingsScreenState extends State<TwoFASettingsScreen> {
         backgroundColor: Colors.transparent,
       ),
       body: SingleChildScrollView(
-          child: ConstrainedBox(
-              constraints: const BoxConstraints(maxWidth: 800),
-              child: Container(
-                  padding: const EdgeInsets.all(30),
-                  child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Container(
-                            padding: const EdgeInsets.symmetric(
-                                horizontal: 20, vertical: 15),
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: [
-                                const Text(
-                                  "Enable 2FA",
-                                  style: TextStyle(
-                                      fontSize: 17,
-                                      fontWeight: FontWeight.bold),
-                                ),
-                                Switch(
-                                  value: twoFaOn,
-                                  activeColor: AppColours.darkBlue,
-                                  onChanged: (bool value) async {
-                                    await updateTwoFactor(
-                                        widget.userId, widget.userRole, value);
-                                    setState(() {
-                                      twoFaOn = value;
-                                    });
-                                  },
-                                )
-                              ],
-                            )),
-                        const SizedBox(height: 10),
-                        greyDividerThick(),
-                        const SizedBox(height: 20),
-                        const Text(
-                            "Two-factor authentication keeps your account safe by asking you to enter a verification code sent to your phone every time you log in.",
-                            style: TextStyle(fontSize: 15)),
-                      ])))),
+          child: Center(
+              child: ConstrainedBox(
+                  constraints: const BoxConstraints(maxWidth: 800),
+                  child: Container(
+                      padding: const EdgeInsets.all(30),
+                      child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Container(
+                                padding: const EdgeInsets.symmetric(
+                                    horizontal: 20, vertical: 15),
+                                child: Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    const Text(
+                                      "Enable 2FA",
+                                      style: TextStyle(
+                                          fontSize: 17,
+                                          fontWeight: FontWeight.bold),
+                                    ),
+                                    Switch(
+                                      value: twoFaOn,
+                                      activeColor: AppColours.darkBlue,
+                                      onChanged: (bool value) async {
+                                        await updateTwoFactor(widget.userId,
+                                            widget.userRole, value);
+                                        setState(() {
+                                          twoFaOn = value;
+                                        });
+                                      },
+                                    )
+                                  ],
+                                )),
+                            const SizedBox(height: 10),
+                            greyDividerThick(),
+                            const SizedBox(height: 20),
+                            const Text(
+                                "Two-factor authentication keeps your account safe by asking you to enter a verification code sent to your phone every time you log in.",
+                                style: TextStyle(fontSize: 15)),
+                          ]))))),
+      bottomNavigationBar: roleBasedBottomNavBar(widget.userRole, context, 4),
     );
   }
 }
