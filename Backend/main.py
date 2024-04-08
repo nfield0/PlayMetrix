63abf6c8a6a4459145671503397b1afc8e3f904a
Michael Flynn <micflynn99@gmail.com>
2024-04-08 14:06:46 +0100

    commit

--- Backend/main.py
+++ Backend/main.py
@@ -761,5 +761,5 @@ def cleanup(db:Session = Depends(get_db)):
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True, log_level="debug")
+    uvicorn.run("main:app", host="127.0.0.1", port=80, reload=True, log_level="debug")
     
